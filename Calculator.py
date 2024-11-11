d5470a0615daef8ed67f2decedb9f0b3021a3935
Татьяна Цихоцкая <tsykhotskaya.tatian@gmail.com>
2024-11-11 21:19:12 +0200

    first

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,30 @@
-prve_cislo = int(input("vloz prve cislo: "))
-znamienko = input("vloz znak pozadovane matematicke operace (+, -, *, /): ")
-druhe_cislo = int(input("vloz druhe cislo: "))
+try:
+    prvni_cislo = int(input("vloz prvni cislo: "))
+except(ValueError):
+    print("vlozil jsi chybnou hodnotu, bude pouzito '1'")
+    prvni_cislo = 1
 
-if znamienko == "+":
-    print(f"soucet cisel {prve_cislo} a {druhe_cislo} je {prve_cislo + druhe_cislo}")
-elif znamienko == "-":
-    print(f"rozdil cisel {prve_cislo} a {druhe_cislo} je {prve_cislo - druhe_cislo}")
-elif znamienko == "*":
-    print(f"soucin cisel {prve_cislo} a {druhe_cislo} je {prve_cislo * druhe_cislo}")
-elif znamienko == "/":
+
+znamenko = input("vloz znak pozadovane matematicke operace (+, -, *, /): ")
+if znamenko not in ["+", "-", "*", "/"]:
+    raise ValueError("zvolil jsi chybne matematickou operaci a program bude ukoncen")
+
+try:
+    druhe_cislo = int(input("vloz druhe coslo: "))
+except(ValueError):
+    print("vlozil jsi chybnou hodnotu, bude pouzito '1'")
+    druhe_cislo = 1
+
+if znamenko == "+":
+    print(f"soucet cisel {prvni_cislo} a {druhe_cislo} je {prvni_cislo + druhe_cislo}")
+elif znamenko == "-":
+    print(f"rozdil cisel {prvni_cislo} a {druhe_cislo} je {prvni_cislo - druhe_cislo}")
+elif znamenko == "*":
+    print(f"soucin cisel {prvni_cislo} a {druhe_cislo} je {prvni_cislo * druhe_cislo}")
+elif znamenko == "/":
     try:
-
-    print(f"podil cisel {prve_cislo} a {druhe_cislo} je {prve_cislo / druhe_cislo}")
+        print(f"podil cisel {prvni_cislo} a {druhe_cislo} je {prvni_cislo / druhe_cislo}")
+    except (ZeroDivisionError):
+        print("chces delit nulou a to nejde")
 else:
     print(f"zvolil jsi chybnou operaci, program bude ukoncen")
