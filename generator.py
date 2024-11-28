ef541e45b5cf2dda55fec2a004fee5debc75fe0d
Татьяна Цихоцкая <tsykhotskaya.tatian@gmail.com>
2024-11-28 20:28:35 +0200

    task 12

---
+++ generator.py
@@ -0,0 +1,20 @@
+#Task12 - Vytvoříte generátor, který bude postupně vracet
+# Albert - máme koncovku "bert".
+# před tu koncovku budou dávány předpony "Al", "Ro", "Hu", "Nor", "Gil"
+#
+# def GeneratorJmen(pred, konc):
+#     #TODO zde implementovat
+#     pass
+#
+# predpony = ["Al", "Ro", "Hu", "Nor", "Gil"] #TODO dát dle svého jména
+# koncovka = "bert" #TODO dát dle svého jména
+# for jmeno in GeneratorJmen(predpony, koncovka):
+#     print(f"Tvé jméno je {jmeno}") # Albert, Robert, Hubert, Norbert, Gilbert
+
+def GeneratorJmen(pred, konc):
+    for p in pred:
+        yield p + konc
+predpony = ["Teti", "Mari", "Teri", "lili"]
+koncovka = "ana"
+for jmeno in GeneratorJmen(predpony, koncovka):
+    print(f"Tvé jméno je {jmeno}")
